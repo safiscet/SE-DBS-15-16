0467f7086dd6d4fb1358fa0f1a118036e2895979
tabea <tabea-schmidt93@web.de>
2015-11-09 21:40:53 +0100

    added wahlkreis_date in csv- und sql-Datei

---
+++ Import/copy_wahlkreis_data.sql
@@ -0,0 +1,2 @@
+﻿COPY wahlkreis FROM 'C:/Users/Bambi/Documents/Uni Master/1. Semester/Datenbanksysteme/SE-DBS-15-16-master/SE-DBS-15-16-master/Import/wahlkreis_data.csv'
+ DELIMITERS ';' CSV HEADER;
