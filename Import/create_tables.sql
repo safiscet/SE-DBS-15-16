af9a66fc67b0425a3556536ff71bfefe6874983a
safiscet <stef.fritsch@gmx.de>
2015-11-09 19:32:13 +0100

    Create-Table-Statement

---
+++ Import/create_tables.sql
@@ -0,0 +1,73 @@
+﻿create table Elector (
+	id 		serial		primary key,
+	name		varchar(60),	
+	sex		char(1)		check (sex = 'm' or sex = 'f'),
+	birthday	date		not null,	
+	wahlkreis	smallint	references Wahlkreis(id));
+
+create table Vote (
+	id		serial		primary key,
+	year		smallint	not null,
+	erststimme	smallint	references Candidate(id),
+	zweitstimme	smallint	references Party(id),
+	wahlkreis	smallint	references Wahlkreis(id));
+
+create table Party (
+	id		smallint	primary key,
+	name		varchar(30)	not null,
+	abkuerzung	varchar(10)	not null
+);
+
+create table PartyInElection (
+	party			integer		references Party(id),
+	year			smallint	not null,			
+	erststimmen		integer,
+	zweitstimmen		integer,
+	wonDistricts		smallint,
+	fiverPercentTaken	boolean,
+	primary key (party, year)
+);
+	
+create table FederalLand (
+	id		smallint	primary key,
+	name		varchar(25)	not null,
+	residents2009	integer		not null check (residents2009 >= 0),
+	residents2013	integer		not null check (residents2013 >= 0)
+);
+
+create table Wahlkreis (
+	id		smallint	primary key,
+	name		varchar(90)	not null,
+	federalLand	smallint	references FederalLand(id)
+);
+
+create table WahlkreisInElection (
+	wahlkreis	smallint	references Wahlkreis(id),
+	year		smallint	not null,
+	residents	integer		not null,
+	winnerParty	smallint	references Party(id),
+	winnerCandidate	smallint	references Candidate(id),
+	primary key (wahlkreis, year)
+);
+	
+create table Candidate (
+	id		integer		primary key,
+	name		varchar(80)	not null,
+	jahrgang	smallint	not null 
+);
+	
+create table CandidateInElection (
+	candidate	integer		references Candidate(id),
+	year		smallint	not null,
+	erststimmen	integer,
+	rank		smallint,
+	party		smallint	references Party(id),
+	wahlkreis	smallint	references Wahlkreis(id),
+	primary key (candidate, year) 
+);
+	
+create table RunsForElection (
+	federalLand	smallint	references FederalLand(id),
+	party		smallint	references Party(id),
+	primary key (federalLand, party) 
+);
