4ac558bae97249994de1b4a4a14225152ba6b6cd
tabea <tabea-schmidt93@web.de>
2015-11-10 17:08:54 +0100

    Added runsForElection als csv- und sql-Datei

---
+++ Import/copy_runsForElection_data.sql
@@ -0,0 +1,2 @@
+﻿COPY runsForElection FROM 'C:/Users/Tabea/Documents/Uni Master/1. Semester/Datenbanksysteme/SE-DBS-15-16-master/SE-DBS-15-16-master/Import/runsForElection_data.csv'
+ DELIMITERS ';' CSV HEADER;
