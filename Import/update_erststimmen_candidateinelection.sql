2702f856a4c90edcc985194e6344f4a15fe392e5
safiscet <stef.fritsch@gmx.de>
2015-11-13 20:58:16 +0100

    Update-Skript für Erststimmen in candidateinelection, Materialisierte Sicht für Party pro Wahlkreis (noch nicht vollständig)

---
+++ Import/update_erststimmen_candidateinelection.sql
@@ -0,0 +1,30 @@
+﻿-- Berechne und update Erststimmen pro Kandidat 2009
+UPDATE candidateinelection c
+SET erststimmen = sub.summe
+FROM (
+SELECT c2.candidate, COUNT(v.id) AS summe
+FROM candidateinelection c2 JOIN vote v on c2.candidate = v.erststimme
+WHERE c2.year = 2009
+AND v.year = 2009
+GROUP BY c2.candidate, c2.wahlkreis
+) sub
+WHERE c.candidate = sub.candidate
+AND c.year = 2009;
+
+-- Berechne und update Erststimmen pro Kandidat 2013
+UPDATE candidateinelection c
+SET erststimmen = sub.summe
+FROM (
+SELECT c2.candidate, COUNT(v.id) AS summe
+FROM candidateinelection c2 JOIN vote v on c2.candidate = v.erststimme
+WHERE c2.year = 2013
+AND v.year = 2013
+GROUP BY c2.candidate, c2.wahlkreis
+) sub
+WHERE c.candidate = sub.candidate
+AND c.year = 2013;
+
+-- Ersetze NULL durch 0 in Erststimmen
+UPDATE candidateinelection
+SET erststimmen = 0
+WHERE erststimmen IS NULL;
---
+++ Import/view_partyinwahlkreis.sql
@@ -0,0 +1,12 @@
+﻿-- Zweitstimmen fehlen noch
+-- auch "leere" Wahlkreise mit reinnehmen?
+DROP MATERIALIZED VIEW IF EXISTS partyinwahlkreis;
+
+CREATE MATERIALIZED VIEW partyinwahlkreis AS (
+	SELECT p.party, v.wahlkreis, p.year, COALESCE(COUNT(v.id),0) AS zweitstimmen
+	FROM partyinelection p, vote v 
+	WHERE p.party = v.zweitstimme
+	AND p.year = v.year
+	GROUP BY p.party, v.wahlkreis, p.year
+	ORDER BY p.party, v.wahlkreis, p.year
+);
