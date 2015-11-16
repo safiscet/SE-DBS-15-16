f25678043b19671817183e6908ebc7e2a237ea3e
safiscet <stef.fritsch@gmx.de>
2015-11-16 18:59:42 +0100

    Neue Relation für tatsächliche Bevölkerungszahlen pro Bundesland

--- Blatt 5/Skripte/raiseNumberOfSeatsParty.sql
+++ Blatt 5/Skripte/raiseNumberOfSeatsParty.sql
@@ -6,17 +6,19 @@ DECLARE
     t integer;
     u integer;
 BEGIN
+    -- update minSeats column
+    UPDATE changeDivisorRaiseParty2013 ch
+    set minSeats = old.minSeats
+    FROM (
+	select mind.party, COALESCE(SUM(mind.seats),0) AS minSeats
+	from changedivisorParty2013 mind
+	group by mind.party
+    ) old
+    WHERE ch.party = old.party;
 
     -- compute the first divisor for every party
        update changeDivisorRaiseParty2013 ch
-       set changeByHalfSeat = (old.minSeats - 0.5)
-       from (
-	  select mind.party, COALESCE(SUM(mind.seats),0) AS minSeats
-	  from changedivisorParty2013 mind
-	  group by mind.party
-       ) old
-       where ch.party = old.party
-       and r = ch.party;
+       set changeByHalfSeat = minSeats - 0.5;
 
        update changeDivisorRaiseParty2013 ch
        set divisorCandidate1 = cast(zweitstimmen
@@ -40,7 +42,11 @@ BEGIN
 
     -- update the raised number of seats with the final divisor
 	update changeDivisorRaiseParty2013
-	set seats = zweitstimmen / resultingdivisor;
+	set seats = round(zweitstimmen / resultingdivisor);
+
+    -- compute the ausgleichsmandate
+	update changeDivisorRaiseParty2013
+	SEt ausgleichsmandate = seats - minSeats;
     
 END;
 $$ LANGUAGE plpgsql;
---
+++ Import/federallandsresidents.sql
@@ -0,0 +1,23 @@
+﻿create table federallandsresidents (
+    id smallint references federalland(id),
+    year smallint NOT NULL,
+    residents int NOT NULL,
+    primary key (id, year)
+);
+insert into federallandsresidents (id, year, residents)
+values  (1,2013,2686085), -- Schleswig-Holstein 1
+	(13,2013,1585032), -- Mecklenburg-Vorpommern 13
+	(2,2013,1559655), -- Hamburg 2
+	(3,2013,7354892), -- Niedersachsen 3
+	(4,2013,575805), -- Bremen 4
+	(12,2013,2418267), -- Brandenburg 12
+	(15,2013,2247673), -- Sachsen-Anhalt 15
+	(11,2013,3025288), -- Berlin 11
+	(5,2013,15895182), -- NRW 5
+	(14,2013,4005278), -- Sachsen 14
+	(6,2013,5388350), -- Hessen 6
+	(16,2013,2154202), -- Thüringen 16
+	(7,2013,3672888), -- Rheinland-Pfalz 7
+	(9,2013,11353264), -- Bayern 9
+	(8,2013,9482902), -- BW 8
+	(10,2013,919402); -- Saarland 10
