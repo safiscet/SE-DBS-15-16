5b684d9ae0b65aecffb307abab99e4bce9e14f4f
tabea <tabea-schmidt93@web.de>
2015-11-18 21:44:06 +0100

    Aufgabenstellung Blatt 6 und erster Anfang mit Views plus updateWahlbeteiligung

---
+++ Blatt 6/updateWahlbeteiligung.sql
@@ -0,0 +1,35 @@
+﻿CREATE OR REPLACE FUNCTION updateWahlbeteiligung() RETURNS void AS $$
+DECLARE
+    r integer;
+BEGIN
+    alter table wahlkreisInElection add wahlbeteiligung decimal(5,4);
+
+    FOR r IN SELECT DISTINCT wahlkreis FROM wahlkreisinelection
+    LOOP
+	update wahlkreisInElection set wahlbeteiligung =
+		(select cast(
+			(select count(*) from vote v 
+			join wahlkreis wks on v.wahlkreis = wks.id
+			where v.wahlkreis = w.wahlkreis and v.year = 2013)/
+			cast(w.residents as decimal(13,4)) 
+			as decimal(5,4)) 
+		from wahlkreisinelection w  
+		where w.year = 2013 and w.wahlkreis = r)
+	where year = 2013 and wahlkreis = r;
+
+	update wahlkreisInElection set wahlbeteiligung =
+		(select cast(
+			(select count(*) from vote v 
+			join wahlkreis wks on v.wahlkreis = wks.id
+			where v.wahlkreis = w.wahlkreis and v.year = 2009)/
+			cast(w.residents as decimal(13,4)) 
+			as decimal(5,4)) 
+		from wahlkreisinelection w  
+		where w.year = 2009 and w.wahlkreis = r)
+	where year = 2009 and wahlkreis = r;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
---
+++ Blatt 6/view_q3wahlkreisuebersicht2013.sql
@@ -0,0 +1,9 @@
+﻿DROP MATERIALIZED VIEW IF EXISTS Q3wahlkreisuebersicht2013 CASCADE;
+
+CREATE MATERIALIZED VIEW Q3wahlkreisuebersicht2013 AS (
+	select wk.name as wahlkreis, w.wahlbeteiligung as wahlbeteiligung,
+	c.name as candidate
+	from wahlkreisinelection w join wahlkreis wk on w.wahlkreis = wk.id 
+	join candidate c on w.winnercandidate = c.id
+	where w.year = 2013 
+);
