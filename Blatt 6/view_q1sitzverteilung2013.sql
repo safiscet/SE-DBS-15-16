565cee830c1504053fc6ee4ec665fe10ef4e81ed
safiscet <stef.fritsch@gmx.de>
2015-11-20 16:26:56 +0100

    View für Sitzverteilung

---
+++ Blatt 6/view_q1sitzverteilung2013.sql
@@ -0,0 +1,8 @@
+﻿DROP VIEW IF EXISTS q1sitzverteilung2013;
+
+CREATE VIEW q1sitzverteilung2013 AS (
+select p.abkuerzung as party, sum(ch.seats) as seats
+from changeDivisorPartyFinal2013 ch join party p on ch.party = p.id
+group by p.id, p.abkuerzung
+order by p.abkuerzung
+);
