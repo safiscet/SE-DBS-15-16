ed50e1ab64cab5dd86f6dfc2094928363266e7bf
tabea <tabea-schmidt93@web.de>
2015-11-21 15:06:28 +0100

    view wahlkreissieger nach jahren 2013 und 2009 aufgeteilt

--- Blatt 6/view_q4wahlkreissieger2009.sql
+++ Blatt 6/view_q4wahlkreissieger2009.sql
@@ -1,12 +1,13 @@
-﻿-- View: q4wahlkreissieger
+﻿-- View: q4wahlkreissieger2009
 
-DROP VIEW IF EXISTS q4wahlkreissieger;
+DROP VIEW IF EXISTS q4wahlkreissieger2009;
 
-CREATE VIEW q4wahlkreissieger AS (
+CREATE VIEW q4wahlkreissieger2009 AS (
  select wk.name as wahlkreis, p1.abkuerzung as winnerPartyErststimmen, 
  p2.abkuerzung as winnerPartyZweitstimmen
  from wahlkreis wk join wahlkreisinelection wie on wk.id = wie.wahlkreis
  join candidateInElection c on wie.winnercandidate = c.candidate
  join party p1 on p1.id = c.party
- join party p2 on p2.id = wie.winnerparty --stimmt das???
+ join party p2 on p2.id = wie.winnerparty
+ where wie.year = 2009
 );
---
+++ Blatt 6/view_q4wahlkreissieger2013.sql
@@ -0,0 +1,13 @@
+﻿-- View: q4wahlkreissieger2013
+
+DROP VIEW IF EXISTS q4wahlkreissieger2013;
+
+CREATE VIEW q4wahlkreissieger2013 AS (
+ select wk.name as wahlkreis, p1.abkuerzung as winnerPartyErststimmen, 
+ p2.abkuerzung as winnerPartyZweitstimmen
+ from wahlkreis wk join wahlkreisinelection wie on wk.id = wie.wahlkreis
+ join candidateInElection c on wie.winnercandidate = c.candidate
+ join party p1 on p1.id = c.party
+ join party p2 on p2.id = wie.winnerparty
+ where wie.year = 2013
+);
