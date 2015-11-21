7384dc0fe6b2db5f739c699efac9448357bb0cfd
tabea <tabea-schmidt93@web.de>
2015-11-21 22:23:24 +0100

    added view for q6 tightest winner and looser for 2009 and 2013 completely

--- Blatt 6/view_q6tightestWinner2009.sql
+++ Blatt 6/view_q6tightestWinner2009.sql
@@ -1,8 +1,8 @@
-﻿-- view: q6tightestWinner2013
+﻿-- view: q6tightestWinner2009
 
-DROP VIEW q6tightestWinner2013;
+--DROP VIEW q6tightestWinner2009;
 
-CREATE OR REPLACE VIEW q6tightestWinner2013 AS
+CREATE OR REPLACE VIEW q6tightestWinner2009 AS
 -- differencePerWahlkreis shows the winner candidate for each wahlkreis
 -- and his party plus the difference, with which he has won
 with differencesPerWahlkreis as (
@@ -10,28 +10,28 @@ select pie.wahlkreis,
 (select p.abkuerzung from wahlkreisInElection wie
 join candidateInElection cie on wie.winnercandidate = cie.candidate
 join party p on p.id = cie.party
-where wie.year = 2013
-and cie.year = 2013
+where wie.year = 2009
+and cie.year = 2009
 and wie.wahlkreis = pie.wahlkreis) as candidateParty,
 (select c.name from wahlkreisInElection wie2
 join candidate c on wie2.winnercandidate = c.id
-where wie2.year = 2013 
+where wie2.year = 2009 
 and wie2.wahlkreis = pie.wahlkreis) as candidatename, 
 max(pie.erststimmen) as first,
 (select max(p1.erststimmen) from partyinwahlkreis p1 
-where p1.year = 2013 
+where p1.year = 2009 
 and p1.wahlkreis = pie.wahlkreis
 and exists (select erststimmen from partyinwahlkreis p2
 where p1.erststimmen < p2.erststimmen and 
-p1.wahlkreis = p2.wahlkreis and p2.year = 2013)) as second,
+p1.wahlkreis = p2.wahlkreis and p2.year = 2009)) as second,
 (select max(pie.erststimmen)- (select max(p1.erststimmen) from partyinwahlkreis p1 
-where p1.year = 2013 
+where p1.year = 2009 
 and p1.wahlkreis = pie.wahlkreis
 and exists (select erststimmen from partyinwahlkreis p2
 where p1.erststimmen < p2.erststimmen and 
-p1.wahlkreis = p2.wahlkreis and p2.year = 2013))) as difference
+p1.wahlkreis = p2.wahlkreis and p2.year = 2009))) as difference
 from partyinwahlkreis pie
-where pie.year = 2013
+where pie.year = 2009
 group by pie.wahlkreis),
 -- rankedDifferencesPerParty ranks the candidates per party beginning
 -- with the smallest difference
@@ -46,15 +46,15 @@ c.name as candidatename, cie.erststimmen as ownErststimmen,
 (select cie3.erststimmen from candidateInElection cie2 
 join wahlkreisInElection wie on cie2.wahlkreis = wie.wahlkreis
 join candidateInElection cie3 on wie.winnerCandidate = cie3.candidate
-where cie2.year = 2013 
-and wie.year = 2013
-and cie3.year = 2013
+where cie2.year = 2009 
+and wie.year = 2009
+and cie3.year = 2009
 and cie2.candidate = cie.candidate) as bestErststimmen
 from partyinelection pie join party p on pie.party = p.id
 join candidateInElection cie on cie.party = p.id
 join candidate c on cie.candidate = c.id
-where pie.year = 2013
-and cie.year = 2013
+where pie.year = 2009
+and cie.year = 2009
 group by p.abkuerzung, c.name, cie.erststimmen, cie.candidate
 having sum(pie.wondistricts) = 0
 order by p.abkuerzung),
