a332dfa2ef0042dbe252c70758589c534c84d7c2
safiscet <stef.fritsch@gmx.de>
2015-12-02 21:02:36 +0100

    Benötigter Import für Bevölkerungszahl pro Bundesland für 2009

--- Import/federallandsresidents.sql
+++ Import/federallandsresidents.sql
@@ -1,11 +1,13 @@
-﻿create table federallandsresidents (
+﻿create table if not exists federallandsresidents (
     id smallint references federalland(id),
     year smallint NOT NULL,
     residents int NOT NULL,
     primary key (id, year)
 );
-insert into federallandsresidents (id, year, residents)
-values  (1,2013,2686085), -- Schleswig-Holstein 1
+delete from federallandsresidents;
+insert into federallandsresidents (id, year, residents) values  
+	-- 2013
+	(1,2013,2686085), -- Schleswig-Holstein 1
 	(13,2013,1585032), -- Mecklenburg-Vorpommern 13
 	(2,2013,1559655), -- Hamburg 2
 	(3,2013,7354892), -- Niedersachsen 3
@@ -20,4 +22,21 @@ values  (1,2013,2686085), -- Schleswig-Holstein 1
 	(7,2013,3672888), -- Rheinland-Pfalz 7
 	(9,2013,11353264), -- Bayern 9
 	(8,2013,9482902), -- BW 8
-	(10,2013,919402); -- Saarland 10
+	(10,2013,919402), -- Saarland 10
+	-- 2009
+	(1,2009,2834300), -- Schleswig-Holstein 1
+	(13,2009,1664400), -- Mecklenburg-Vorpommern 13
+	(2,2009,1772100), -- Hamburg 2
+	(3,2009,7947200), -- Niedersachsen 3
+	(4,2009,661900), -- Bremen 4
+	(12,2009,2522500), -- Brandenburg 12
+	(15,2009,2381900), -- Sachsen-Anhalt 15
+	(11,2009,3431700), -- Berlin 11
+	(5,2009,17933100), -- NRW 5
+	(14,2009,4192800), -- Sachsen 14
+	(6,2009,6065000), -- Hessen 6
+	(16,2009,2267800), -- Thüringen 16
+	(7,2009,4028400), -- Rheinland-Pfalz 7
+	(9,2009,12519700), -- Bayern 9
+	(8,2009,10749500), -- BW 8
+	(10,2009,1030300); -- Saarland 10
