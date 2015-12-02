447ac63a193f9cc8e7b6799c188596ac49c23a91
safiscet <stef.fritsch@gmx.de>
2015-12-02 21:04:04 +0100

    Ideen für Indizes eingefügt

---
+++ Indizes.sql
@@ -0,0 +1,12 @@
+﻿CREATE INDEX ON candidateinelection (party);
+CREATE INDEX ON candidateinelection (wahlkreis);
+CREATE INDEX ON candidateinelection (federalland);
+
+CREATE INDEX ON vote (year);
+CREATE INDEX ON vote (wahlkreis);
+
+CREATE INDEX ON wahlkreisinelection (winnerparty);
+CREATE INDEX ON wahlkreisinelection (winnercandidate);
+
+-- Sinnvoll? CREATE INDEX ON vote (erststimme);
+-- Sinnvoll? CREATE INDEX ON vote (zweitstimme);
