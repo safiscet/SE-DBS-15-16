171b66bd39b777fa06e29745e5fda85783eaab3b
tabea <tabea-schmidt93@web.de>
2015-12-06 13:21:52 +0100

    added some indices

--- Indizes.sql
+++ Indizes.sql
@@ -5,8 +5,14 @@ CREATE INDEX ON candidateinelection (federalland);
 CREATE INDEX ON vote (year);
 CREATE INDEX ON vote (wahlkreis);
 
+CREATE INDEX ON wahlkreisinelection (wahlkreis);
 CREATE INDEX ON wahlkreisinelection (winnerparty);
 CREATE INDEX ON wahlkreisinelection (winnercandidate);
+
+CREATE INDEX ON partyinelection (party);
+
+CREATE INDEX ON partyinwahlkreis (wahlkreis);
+CREATE INDEX ON partyinwahlkreis (party);
 
 -- Sinnvoll? CREATE INDEX ON vote (erststimme);
 -- Sinnvoll? CREATE INDEX ON vote (zweitstimme);
