283534c90223db2dcd732f499a89154e61d86254
tabea <tabea-schmidt93@web.de>
2015-12-16 16:29:26 +0100

    1. Versuch Farben der Partei zu ändern

--- Blatt 6/view_q1sitzverteilung2009.sql
+++ Blatt 6/view_q1sitzverteilung2009.sql
@@ -1,8 +1,8 @@
 ﻿DROP VIEW IF EXISTS q1sitzverteilung2009;
 
 CREATE VIEW q1sitzverteilung2009 AS (
-select p.abkuerzung as party, sum(ch.seats) as seats
+select p.abkuerzung as party, p.color as color, sum(ch.seats) as seats
 from calculateResult(2009) ch join party p on ch.party = p.id
-group by p.id, p.abkuerzung
+group by p.id, p.abkuerzung, p.color
 order by p.abkuerzung
 );
---
+++ alterTableParty.sql
@@ -0,0 +1,13 @@
+﻿alter table party add color varchar(30);
+
+update party set color = 'black' where abkuerzung = 'CDU';
+
+update party set color = 'red' where abkuerzung = 'SPD';
+
+update party set color = 'green' where abkuerzung = 'GRUENE';
+
+update party set color = 'yellow' where abkuerzung = 'FDP';
+
+update party set color = 'purple' where abkuerzung = 'DIE LINKE';
+
+update party set color = 'blue' where abkuerzung = 'CSU';
