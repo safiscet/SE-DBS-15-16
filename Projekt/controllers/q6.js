c9f64a55816c31534c66eb97fd3d3d623e087c7f
tabea <tabea-schmidt93@web.de>
2015-12-18 16:44:21 +0100

    Q6 Sieger und Verlierer in Farbe und mit Plus bzw. Minus, auf Anfangsseite Tabelle ausgeblendet

--- Projekt/controllers/q6.js
+++ Projekt/controllers/q6.js
@@ -18,16 +18,18 @@ exports.loadQ6 = function (req, res) {
     var query2 = client.query("SELECT p.id, p.abkuerzung as party FROM partyinelection e, party p WHERE e.party = p.id AND e.year = 2013 ORDER BY p.abkuerzung");
 
     if(partyId == undefined){
-      var query = client.query("SELECT * FROM q6tightestwinner2013");
+      //var query = client.query("SELECT * FROM q6tightestwinner2013");
     }
     else {
       var query = client.query("SELECT * FROM q6tightestwinner2013 q, party p WHERE q.candidateparty = p.abkuerzung AND p.id ="+partyId);
+
+      // Stream results back one row at a time
+      query.on('row', function(row) {
+        results.push(row);
+      });
     }
 
-    // Stream results back one row at a time
-    query.on('row', function(row) {
-      results.push(row);
-    });
+
 
     query2.on('row', function(row) {
       parties.push(row);
