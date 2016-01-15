a41aaac5e681f83ab1109f430b355085bf870186
tabea <tabea-schmidt93@web.de>
2016-01-15 14:34:50 +0100

    Query geändert, Parteien, die in beiden Wahlen null Stimmen haben, werden nicht mehr angezeigt, Parteien, die nur an einer Wahl teilgenommen haben, werden nun mit angezeigt, zusätzliche Spalte Vergleich zum jeweils anderen Jahr

--- Projekt/controllers/q3.js
+++ Projekt/controllers/q3.js
@@ -38,7 +38,9 @@ exports.loadQ3 = function (req, res) {
         errorTable.push("Der gewählte Wahlkreis existiert nicht. Es werden Beispiel-Ergebnisse für Flensburg - Schleswig angezeigt.");
       }
       var query = client.query("SELECT * FROM q3wahlkreisuebersicht"+year+" WHERE nummer = $1", [wahlkreisInt]);
-      var query2 = client.query("SELECT q13.wahlkreis, q13.party, q13.zweitstimmenabsolute, q13.zweitstimmenpercent, q9.zweitstimmenabsolute2009, q9.zweitstimmenpercent2009 FROM q3wahlkreisparty2013 q13 JOIN q3wahlkreisparty2009 q9 ON q13.wahlkreis = q9.wahlkreis JOIN wahlkreis w ON w.name = q13.wahlkreis WHERE q13.party = q9.party AND w.id = $1", [wahlkreisInt]);
+      //var query2 = client.query("SELECT q13.wahlkreis, q13.party, q13.zweitstimmenabsolute, q13.zweitstimmenpercent, q9.zweitstimmenabsolute2009, q9.zweitstimmenpercent2009 FROM q3wahlkreisparty2013 q13 JOIN q3wahlkreisparty2009 q9 ON q13.wahlkreis = q9.wahlkreis JOIN wahlkreis w ON w.name = q13.wahlkreis WHERE q13.party = q9.party AND w.id = $1", [wahlkreisInt]);
+
+      var query2 = client.query("with q13 as (select * from q3wahlkreisparty2013 q join wahlkreis w on w.name = q.wahlkreis	where w.id = $1), q9 as (select * from q3wahlkreisparty2009 q join wahlkreis w	on w.name = q.wahlkreis	where w.id = $1)	select greatest(q13.wahlkreis, q9.wahlkreis) as wahlkreis, greatest(q13.party, q9.party) as party, q13.zweitstimmenabsolute,  q13.zweitstimmenpercent, q9.zweitstimmenabsolute2009, q9.zweitstimmenpercent2009 from q13 FULL OUTER JOIN q9 ON q13.party = q9.party WHERE NOT (q13.zweitstimmenabsolute = 0 AND q9.zweitstimmenabsolute2009 = 0) order by party", [wahlkreisInt]);
 
       // Stream results back one row at a time
       query.on('row', function(row) {
